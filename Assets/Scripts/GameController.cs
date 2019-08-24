e29a2e771eca5808b21cc7653ed05851e24be944
Jonathan Bouligny <jonathanbouligny@protonmail.com>
2019-08-24 10:56:49 -0500

    Added old scripts and created new scripts
    Added Data Manager, EnemyBlockSpawner, GameController, PlayerController, PlankRotationController,  and BlockSpawnPointChecker

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //State machine
+    // Start is called before the first frame update
+    public bool playingGame;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
